0fae4ffc7765563163f5beaed728e422603b1fc7
Aina Merchant <ainamerchant10@gmail.com>
2023-09-25 16:27:34 -0400

    "Implemented GetAverageGradeUseCase.java"

--- src/main/java/use_case/GetAverageGradeUseCase.java
+++ src/main/java/use_case/GetAverageGradeUseCase.java
@@ -12,6 +12,14 @@ public final class GetAverageGradeUseCase {
 
     public float getAverageGrade(String course) {
         // TODO: Get average grade for all students in your team.
-        return 0.0f;
+        Team team = gradeDB.getMyTeam();
+        String [] members = team.getMembers();
+        int grade = 0;
+        int count = 0;
+        for (String i : members){
+            grade += gradeDB.getGrade(i,course).getGrade();
+            count+=1;
+        }
+        return (float) grade /count;
     }
 }
