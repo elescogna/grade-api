694d915687c1fb8fe5289cbbc679d61d22f53c34
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2023-09-25 16:26:44 -0400

    implemented getMyTeam

--- src/main/java/api/MongoGradeDB.java
+++ src/main/java/api/MongoGradeDB.java
@@ -186,10 +186,35 @@ public class MongoGradeDB implements GradeDB {
     }
 
     @Override
-    // TODO: Implement this method
-    //       Hint: Read apiDocuments/getMyTeam.md and refer to the above
-    //             methods to help you write this code (copy-and-paste + edit as needed).
     public Team getMyTeam() {
-        return null;
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("application/json");
+        JSONObject requestBody = new JSONObject();
+        RequestBody body = RequestBody.create(mediaType, requestBody.toString());
+        Request request = new Request.Builder()
+                .url("https://grade-logging-api.chenpan.ca/team")
+                .method("GET", body)
+                .addHeader("Authorization", API_TOKEN)
+                .addHeader("Content-Type", "application/json")
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            System.out.println(response);
+            JSONObject responseBody = new JSONObject(response.body().string());
+
+            if (responseBody.getInt("status_code") == 200) {
+                JSONObject team = responseBody.getJSONObject("team");
+                JSONArray members = team.getJSONArray("members");
+                String name = team.getString("name");
+                return new Team(name, (String[]) (members.toList()).toArray());
+            } else {
+                throw new RuntimeException(responseBody.getString("message"));
+            }
+        }
+        catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
